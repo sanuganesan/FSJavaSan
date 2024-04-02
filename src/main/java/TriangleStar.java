45a4fc6cd33449c11023ab42de1b0c087a2817ba
Sangeetha <sangeetha060688@gmail.com>
2024-04-03 00:29:07 +0530

    update 5_StarTriangle

---
+++ src/main/java/TriangleStar.java
@@ -0,0 +1,26 @@
+public class TriangleStar {
+
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= n - i; k++) {
+                System.out.print(" ");
+
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+
+            }
+
+            for (int m = 2; m <= i; m++) {
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
+        }
+
+    }
+}
